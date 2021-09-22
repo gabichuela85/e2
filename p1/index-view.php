b94a573105642560de0ba388e942e44e976dd1ca
Gabichuela85 <gaby.leonguerrero@gmail.com>
2021-09-22 02:01:47 +0000

    updated the phpiggybank for the assignment 3, worked on project

---
+++ p1/index-view.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Project 1</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+</head>
+
+<body>
+    <h1>Project 1</h1>
+
+    <h2>Mechanics</h2>
+    <ul>
+        <li>Player A and Player B randomly "throw" either Rock, Paper, Scissors, Lizard, or Spock.</li>
+        <li>A tie is declared if both players throw the same move.</li>
+        <li>Otherwise:</li>
+        <ul>
+            <li>Scissors cuts Paper</li>
+            <li>Paper covers Rock</li>
+            <li>Rock crushes Lizard</li>
+            <li>Lizard poisons Spock</li>
+            <li>Spock smashes Scissors</li>
+            <li>Scissors decapitates Lizard</li>
+            <li>Lizard eats Paper</li>
+            <li>Paper disproves Spock</li>
+            <li>Spock vaporizes Rock</li>
+            <li>Rock crushes Scissors</li>
+        </ul>
+    </ul>
+</body>
+
+</html>
--- p1/index.php
+++ p1/index.php
@@ -1,17 +1,5 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <title>Project 1</title>
-    <meta charset='utf-8'>
-    <link href=data:, rel=icon>
-</head>
-<body> 
-    <h1>Project 1</h1>
-
-    <h2>Mechanics</h2>
-    <ul>
-        <li>...</li>
-    </ul>
-</body>
-</html> 
+<?php
+// Rock Paper Scissors Lizard Spock Game Simulator 
+// per Big Bang Theory 
+//
+require 'index-view.php'; 
